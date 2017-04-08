ef748a6070de0827efefb41f1a8c5b1c291d8f37
Brad Richardson <brichardson@structint.com>
2017-04-07 19:44:53 -0700

    Create a function to flip a piece

--- Piece.hs
+++ Piece.hs
@@ -23,3 +23,10 @@ appendPieces (Piece in_dir1 in_vec1 out_dir1)
                 (Piece in_dir1 (addVecs in_vec1 new_vec) new_out_dir)
 
 addVecs (x1, y1, z1) (x2, y2, z2) = (x1 + x2, y1 + y2, z1 + z2)
+
+flipPiece (Piece in_dir (x, y, z) out_dir) = let (rotated_x, rotated_y, rotated_z) = rotate (x, y, z) (-in_dir)
+                                                 (unrotated_x, unrotated_y, unrotated_z) = rotate (rotated_x, (-rotated_y), rotated_z) in_dir
+                                                 rotated_out_dir = out_dir - in_dir
+                                                 flipped_out_dir = (-rotated_out_dir)
+                                                 unrotated_out_dir = flipped_out_dir + in_dir in
+                                            (Piece in_dir (unrotated_x, unrotated_y, unrotated_z) unrotated_out_dir)
